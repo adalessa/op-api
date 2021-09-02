8d0d5869bc131b194cd09e2f43422e90fb572b54
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2021-09-02 22:29:18 +0200

    Agregamos nueva vista, y request de ejemplo

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,10 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| agregada
 |
 */
 
 Route::get('/', function () {
-    return "OP-API";
+    return view('home');
 });
